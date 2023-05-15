1aff69793af1327af61416f1a0ab758eaf64cef3
redstone <che0618@naver.com>
2023-05-15 09:40:03 +0900

    .

--- baekjoon/class.1/1157.js
+++ baekjoon/class.1/1157.js
@@ -1,8 +1,21 @@
 // let input = fs.readFileSync("/dev/stdin").toString();
 const fs = require("fs");
 
-let input = fs.readFileSync("data.txt").toString().trim().toUpperCase();
+let input = fs
+  .readFileSync("data.txt")
+  .toString()
+  .trim()
+  .toUpperCase()
+  .split("");
 
 let answer = input.sort();
 
-console.log(answer);
+let count = []        
+for(let i  = 0; i < answer.length; i++) {
+  let str = answer[i]
+  if(str == answer[i]){
+    count += 1
+    if (count > realCount) {}
+  }
+  if (){}
+}
