44bd2ef84126621271289d72765c045ca5caa234
redstone <che0618@naver.com>
2023-07-24 09:32:03 +0900

    7.21

--- baekjoon/class.1/10250.js
+++ baekjoon/class.1/10250.js
@@ -1,9 +1,9 @@
-// let input = fs.readFileSync("/dev/stdin").toString();
+// let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 let input = require("fs")
   .readFileSync("data.txt")
   .toString()
   .trim()
-  .split("\r\n");
+  .split("\n");
 
 for (let i = 1; i < input.length; i++) {
   let hotel = input[i].split(" ");
@@ -11,12 +11,12 @@ for (let i = 1; i < input.length; i++) {
     let x = Math.ceil(hotel[2] / hotel[0]);
     let y = hotel[2] % hotel[0];
     if (y === 0) {
-      y = Number(hotel[0]);
+      y = hotel[0];
     }
     if (x < 10) {
       x = "0" + x;
     }
-    console.log(y + x);
+    console.log(String(y) + String(x));
   } else {
     console.log(hotel[2] + "01");
   }
---
+++ baekjoon/class.1/27866.js
@@ -0,0 +1,6 @@
+// let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+let input = require("fs")
+  .readFileSync("data.txt")
+  .toString()
+  .trim()
+  .split("\n");
