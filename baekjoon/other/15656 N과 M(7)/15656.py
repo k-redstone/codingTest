a9ab9578b94e3b9dc0f4fbae2bd09159004ec9bc
k-redstone <che06189@gmail.com>
2024-02-13 16:01:55 +0900

    Feat: 24.02.13 15656 N과 M(7) 풀이

---
+++ baekjoon/other/15656 N과 M(7)/15656.py
@@ -0,0 +1,13 @@
+import sys
+N, M = map(int, sys.stdin.readline().rstrip().split())
+case_list = sorted(list(map(int, input().split())))
+print_list = [0]*M
+def backtrack(idx):
+    if idx >= M:
+        print(*print_list)
+        return
+    for num in case_list:
+        print_list[idx] = num
+        backtrack(idx+1)
+        print_list[idx] = 0
+backtrack(0)
