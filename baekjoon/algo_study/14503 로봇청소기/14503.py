64df75f18868c51256348726ad47bda6920ac144
k-redstone <che0618@naver.com>
2024-02-14 23:06:36 +0900

    Feat: 24.02.14 14503 로봇청소기 풀이

--- baekjoon/algo_study/14503 로봇청소기/14503.py
+++ baekjoon/algo_study/14503 로봇청소기/14503.py
@@ -1,86 +1,41 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-'''
-1.현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다.
-2.현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 없는 경우,
-    1.바라보는 방향을 유지한 채로 한 칸 후진할 수 있다면 한 칸 후진하고 1번으로 돌아간다.
-    2.바라보는 방향의 뒤쪽 칸이 벽이라 후진할 수 없다면 작동을 멈춘다.
-3. 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 있는 경우,
-    1. 반시계 방향으로 90도 회전한다.
-    2. 바라보는 방향을 기준으로 앞쪽 칸이 청소되지 않은 빈 칸인 경우 한 칸 전진한다.
-    3. 1번으로 돌아간다.
-'''
+d_row, d_col = [-1, 0, 1, 0], [0, 1, 0, -1]
 
-d_row, d_col = [1, 0, -1, 0], [0, 1, 0, -1]
+def can_clean(row, col):
+    return 0 <= row < N and 0 <= col < M and case_matrix[row][col] == 0
+
+def rotate_left(direction):
+    return (direction - 1) % 4
+
+def move_back(row, col, direction):
+    return row + d_row[(direction + 2) % 4], col + d_col[(direction + 2) % 4]
+
 N, M = map(int, input().split())
 start_row, start_col, direction = map(int, input().split())
 case_matrix = [list(map(int, input().split())) for _ in range(N)]
-
 cnt = 0
-temp = 0
-
-
-def method_1(arr, direction):
-    if direction == 0:
-        if case_matrix[arr[2][0]][arr[2][1]] == 1:
-            return False
-        return arr[2]
-    if direction == 1:
-        if case_matrix[arr[3][0]][arr[3][1]] == 1:
-            return False
-        return arr[3]
-    if direction == 2:
-        if case_matrix[arr[0][0]][arr[0][1]] == 1:
-            return False
-        return arr[0]
-    if direction == 3:
-        if case_matrix[arr[1][0]][arr[1][1]] == 1:
-            return False
-        return arr[1]
-    
-def method_2(row, col, direction):
-    if direction == 0:
-        if case_matrix[row + d_row[1]][col +d_col[1]] == 0:
-            return (row + d_row[1], col +d_col[1], 1)
-        return (row, col, 1)
-    if direction == 1:
-        if case_matrix[row + d_row[2]][col +d_col[2]] == 0:
-            return (row + d_row[2], col +d_col[2], 2)
-        return (row, col, 2)
-    if direction == 2:
-        if case_matrix[row + d_row[3]][col +d_col[3]] == 0:
-            return (row + d_row[3], col +d_col[3], 3)
-        return (row, col, 3)
-    if direction == 3:
-        if case_matrix[row + d_row[0]][col +d_col[0]] == 0:
-            return (row + d_row[0], col +d_col[0], 0)
-        return (row, col, 0)
 
 while True:
     if case_matrix[start_row][start_col] == 0:
         cnt += 1
-        print(cnt)
         case_matrix[start_row][start_col] = 2
-    change =False
-    check_list = []
-    for num in range(4):
-        new_row = start_row + d_row[num]
-        new_col = start_col + d_col[num]
-        if 0 <= new_row < N and 0 <= new_col < M:
-            check_list.append((new_row,new_col))
-            if case_matrix[new_row][new_col] == 0:
-                change = True
-    if not change:
-        result = method_1(check_list, direction)
-        if not result:
-            break
-        start_row, start_col = result
-    else:
-        print('change')
-        start_row, start_col, direction = method_2(start_row, start_col, direction)
 
-    print('--------------------')
-    print(start_row, start_col)
+    cleaned = False
+    for _ in range(4):
+        next_direction = rotate_left(direction)
+        next_row, next_col = start_row + d_row[next_direction], start_col + d_col[next_direction]
+        if can_clean(next_row, next_col):
+            start_row, start_col, direction = next_row, next_col, next_direction
+            cleaned = True
+            break
+        direction = next_direction
+
+    if not cleaned:
+        back_row, back_col = move_back(start_row, start_col, direction)
+        if case_matrix[back_row][back_col] == 1:
+            break
+        start_row, start_col = back_row, back_col
+
 print(cnt)
