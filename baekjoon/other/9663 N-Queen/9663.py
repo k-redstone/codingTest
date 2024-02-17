fde9d5f8be15c0809d920975d7c1ac066dada7d5
k-redstone <che0618@naver.com>
2024-02-17 16:44:25 +0900

    Feat: 24.02.17 N-queen 풀이

--- baekjoon/other/9663 N-Queen/9663.py
+++ baekjoon/other/9663 N-Queen/9663.py
@@ -12,49 +12,35 @@ delta =[
     (1, 1)
 ]
 
-def check(row, col):
-    return_list = []
-    for item in delta:
-        temp = 1
-        while True:
-            new_row , new_col = row + (item[0]*temp), col + (item[1]*temp)
-            if 0 <= new_row < N and 0 <= new_col < N:
-                return_list.append((new_row, new_col))
-                temp += 1
-            else:
-                break
-    return return_list
-
-def change(case, num):
-    for row, col in case:
-        matrix[row][col] = num
+# def check(row, col):
+#     for item in delta:
+#         temp_row, temp_col = row, col
+#         while True:
+#             temp_row , temp_col = temp_row + item[0], temp_col + item[1]
+#             if 0 <= temp_row < N and 0 <= temp_col < N:
+#                 if matrix[temp_row][temp_col] == 1:
+#                     return False
+#             else:
+#                 break
+#     return True
 
 
-def backtrack(idx):
+def backtrack(row):
     global cnt
-    if idx == N:
+    if row == N:
         cnt += 1
         return
-    for item in matrix:
-        if 0 not in item:
-            return
-    for row in range(N):
-        for col in range(N):
-            if matrix[row][col] == 0:
-                matrix[row][col] = 1
-                check_list = check(row, col)
-                change(check_list, 1)
-                print(check_list)
-                backtrack(idx+1)
-                matrix[row][col] = 0
-                change(check_list, 0)
-                print(check_list)
-                
+    for col in range(N): 
+        if not col_visit[col] and not left_visit[row + col] and not right_visit[row - col + N -1]:
+            col_visit[col] = left_visit[row + col] = right_visit[row - col + N -1] = 1
+            backtrack(row + 1)
+            col_visit[col] = left_visit[row + col] = right_visit[row - col + N -1] = 0
+            
 
-cnt = 0
 N = int(input())
-matrix = [[0]*N for _ in range(N)]
+cnt = 0
+col_visit = [0] * N
+left_visit  = [0] * (2 * N - 1)
+right_visit  = [0] * (2 * N - 1) 
 backtrack(0)
 print(cnt)
-# for i in matrix:
-#     print(*i)
