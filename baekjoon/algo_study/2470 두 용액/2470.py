5ce3c98af3558d2e09456d527b1c969b0882a148
k-redstone <che0618@naver.com>
2024-02-20 20:35:37 +0900

    Feat: 24.02.20 2470 두 용액 풀이

--- baekjoon/algo_study/2470 두 용액/2470.py
+++ baekjoon/algo_study/2470 두 용액/2470.py
@@ -3,24 +3,19 @@ input = sys.stdin.readline
 
 N = int(input())
 case_list = sorted(map(int, input().split()))
-start_point = 0
-end_point = N -1
+start_point, end_point = 0, N -1
+sum_list= []
 
-min_num = 2000000000
-cnt = 0
-while start_point != N-1:
-    if start_point == end_point:
-        start_point += 1
-        end_point = N-1
-        continue
+while start_point != end_point:
     sum_num = case_list[start_point] + case_list[end_point]
+    if sum_num > 0:
+        sum_list.append((sum_num, case_list[start_point], case_list[end_point]))
+        end_point -= 1
+    if sum_num < 0:
+        sum_list.append((sum_num, case_list[start_point], case_list[end_point]))
+        start_point += 1
     if sum_num == 0:
-        result_start, result_end = start_point, end_point
+        sum_list.append((sum_num, case_list[start_point], case_list[end_point]))
         break
-    if abs(sum_num) < abs(min_num):
-        min_num = sum_num
-        result_start, result_end = start_point, end_point
-    end_point -= 1
-    cnt+=1
-print(cnt)
-print(case_list[result_start], case_list[result_end])
+sum_list = sorted(sum_list, key=lambda item: abs(item[0]))
+print(sum_list[0][1], sum_list[0][2])
