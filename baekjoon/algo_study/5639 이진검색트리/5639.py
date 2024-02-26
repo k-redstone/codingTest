3c93d4c91433ffac0297ab4b28a0d732f673880f
redstone <che0618@naver.com>
2024-02-27 00:59:16 +0900

    Fix: 5639 시간초과...

--- baekjoon/algo_study/5639 이진검색트리/5639.py
+++ baekjoon/algo_study/5639 이진검색트리/5639.py
@@ -1,22 +1,38 @@
 import sys
-input = sys.stdin.readline
-
-def post_order(node):
-    if 1 <= node < len(tree):
-        # print(node)
-        post_order(node+1)
-        post_order(node+2)
-        print(tree[node])
+sys.stdin = open("./baekjoon/algo_study/5639 이진검색트리/input.txt", "r")
+# input = sys.stdin.readline
 
 
-tree = []
+def make_binary_tree(root, node):
+    if node:
+        if root > node:
+            if dict_tree[root][0] == 0:
+                dict_tree[root][0] = node
+                return
+            else:
+                make_binary_tree(dict_tree[root][0], node)
+        if root < node:
+            if dict_tree[root][1] == 0:
+                dict_tree[root][1] = node
+                return
+            else:
+                make_binary_tree(dict_tree[root][1], node)
+
+def post_order(node):   
+    if node:
+        post_order(dict_tree[node][0])
+        post_order(dict_tree[node][1])
+        print(node, sep="\n")
+
+case_list = []
 while True:
     try:
-        node = [int(input()), ]
-        tree.append(node)
+        node = int(input())
+        case_list.append(node)
     except:
         break
-
-print(tree)
-
-post_order(1)
+dict_tree = {i: [0, 0] for i in case_list}
+root = case_list[0]
+for item in case_list:
+    make_binary_tree(root, item)
+post_order(root)
