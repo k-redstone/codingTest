d5dd21351d34aa506807b3ba239003eee32a084f
k-redstone <che0618@naver.com>
2024-02-27 22:27:10 +0900

    Feat: 24.02.27 1918 후위표기식 풀이

--- baekjoon/algo_study/1918 후위 표기식/1918.py
+++ baekjoon/algo_study/1918 후위 표기식/1918.py
@@ -2,20 +2,12 @@ import sys
 input = sys.stdin.readline
 
 
-
-
-icp = {'(':3, '*':2, '/':2, '+':1, '-':1}
-isp = {'(':0, '*':2, '/':2, '+':1, '-':1}
-
-
-
 def trans(case_list):
     expression = []
     sign = []
     for item in case_list:
         if item not in signs:
             expression.append(item)
-
         else:
             if len(sign) == 0:
                 sign.append(item)
@@ -26,24 +18,21 @@ def trans(case_list):
                         if pop_item == '(':
                             break
                         expression.append(pop_item)
-                elif icp[item] < isp[sign[-1]]:
-                    while icp[item] < isp[sign[-1]]:
+                elif icp[item] <= isp[sign[-1]]:
+                    while icp[item] <= isp[sign[-1]]:
                         expression.append(sign.pop())
+                        if not sign:
+                            break
+                    sign.append(item)
                 else:
                     sign.append(item)
-
     while sign:
         expression.append(sign.pop())
     return ''.join(expression)
 
 
-
-
-
 case_list = list(input().rstrip())
 signs = ['(', ')', '+', '-', '*', '/']
-
-stack= []
-
-
+icp = {'(':3, '*':2, '/':2, '+':1, '-':1}
+isp = {'(':0, '*':2, '/':2, '+':1, '-':1}
 print(trans(case_list))
