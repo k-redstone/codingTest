532def21313a8a7e78680f7bd5a64a74649eea67
che0618 <che0618@naver.com>
2024-03-06 17:43:08 +0900

    Feat: 24.03.06 1987 알파벳 풀이

--- baekjoon/algo_study/1987 알파벳/1987.py
+++ baekjoon/algo_study/1987 알파벳/1987.py
@@ -3,23 +3,23 @@ input = sys.stdin.readline
 
 d_row, d_col = [-1, 1, 0, 0], [0, 0, -1, 1]
 
-def backtrack(row, col, dist):
+def backtrack(row, col, dist, visited):
     global answer
-    if case_matrix[row][col] in stack:
+    if case_matrix[row][col] in visited:
         answer = max(answer, dist)
         return
     
-    stack.append(case_matrix[row][col])
+    visited.add(case_matrix[row][col])
     for idx in range(4):
         new_row, new_col = row+d_row[idx], col+d_col[idx]
         if 0 <= new_row < R and 0 <= new_col < C:
-            backtrack(new_row, new_col, dist+1)
-    stack.pop()
+            backtrack(new_row, new_col, dist+1, visited)
+    visited.remove(case_matrix[row][col])
 
 R, C = map(int, input().rstrip().split())
 case_matrix = [list(input().rstrip()) for _ in range(R)]
 
 stack = []
 answer = 1
-backtrack(0, 0, 0)
+backtrack(0, 0, 0, set())
 print(answer)
